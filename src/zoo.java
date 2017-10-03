8c72b7b35281c8f59d0783808e53e2c233dcba6a
Sarah Mosley <SarahMosley1@students.berkeley.net>
2017-10-03 10:20:08 -0700

    Initial commit

---
+++ src/zoo.java
@@ -0,0 +1,24 @@
+/**
+ * Created by h205p2 on 10/2/17.
+ */
+public class zoo {
+    public static void main(String[] args) {
+        Tiger tigger = new Tiger("tigger");
+        tigger.sleep();
+        tigger.eat("sarah");
+    }
+
+    public void sleep(String name) {
+
+        System.out.println(name + " sleeps for 8 hours");
+    }
+
+    String favoriteFood = "bacon";
+
+    public void eat(String name, String food) {
+        System.out.println(name + " eats " + food);
+        if (food.equals(favoriteFood)) {
+            System.out.println("YUM!!! " + name + " wants more " + food);
+        }
+    }
+}
