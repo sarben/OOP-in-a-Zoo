fe3216acc1ddc08ab4739f2a7106b67f47dd0f10
Sarah Mosley <SarahMosley1@students.berkeley.net>
2017-10-04 10:31:42 -0700

    W

--- src/Bear.java
+++ src/Bear.java
@@ -1,28 +1,27 @@
 /**
  * Created by h205p2 on 10/3/17.
  */
-public class Tiger {
+public class Bear {
     String name;
     String favoriteFood;
 
-    public Tiger(String name) {
+    public Bear(String name) {
         // put your constructor content here
         this.name = name;
-        this.favoriteFood = "meat";
+        this.favoriteFood = "fish";
     }
 
     public void sleep() {
         // complete your sleep function here, noting the change from global to instance variables
-        System.out.println(name +" sleeps for 8 hours");
+        System.out.println(name +" hibernates for 4 months");
     }
-
     public void eat(String food) {
         // complete your eat function here!
         System.out.println(name + " eats " + food);
         if (food.equals(favoriteFood)){
             System.out.println("YUM!!! " + name + " wants more " + food);
         } else {
-            this.sleep();
+        this.sleep();
         }
     }
 }
--- src/zoo.java
+++ src/zoo.java
@@ -2,12 +2,14 @@
  * Created by h205p2 on 10/2/17.
  */
 
-// YEA BOIIIIIIIIIIIII
 public class zoo {
     public static void main(String[] args) {
-        Tiger tigger = new Tiger("tigger");
-        tigger.sleep();
-        tigger.eat("sarah");
+        Tiger tigger = new Tiger("Tigger");
+        tigger.eat("meat");
+        tigger.eat("kibble");
+        Bear pooh = new Bear("Pooh");
+        pooh.eat("fish");
+        pooh.eat("meat");
     }
 
     public void sleep(String name) {
